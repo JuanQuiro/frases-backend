995945cd80575c24a0d6a30ccab3ee86b5384de6
JuanQuiro <juanquirozsanchez@hotmail.com>
2024-08-11 20:05:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const fs = require('fs')
+
+app.use(express.json())
+
+let rawData = fs.readFileSync('frases.json')
+let frasesData = JSON.parse(rawData)
+let frases = frasesData.frases
+
+
+app.get('/api/frases', (req,res) => {
+    const randomIndex = Math.floor(Math.random() * frases.length)
+    res.json({ frases: frases[randomIndex]})
+})
+
+app.listen(port,() => {
+    console.log(`Server running at http://localhost:${port}`)
+})
